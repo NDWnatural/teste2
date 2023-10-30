267e16cbb0cc82d81b9a636fede0d616c8a12ffb
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-10-30 10:23:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-// Substitua 'SUA_CHAVE_DE_API_DO_YOUTUBE' pela sua chave de API real
-const apiKey = 'AIzaSyAiw_Ano92YzywXpv_pPPWHNF8txNpOGsk';
-const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKey = 'SUA_CHAVE_DE_API_DO_YOUTUBE'; // Substitua pela sua chave de API real
+    const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
 
-const resultsContainer = document.getElementById('results');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
+    const resultsContainer = document.getElementById('results');
+    const searchInput = document.getElementById('search-input');
+    const searchButton = document.getElementById('search-button');
 
-searchButton.addEventListener('click', () => {
-    const searchQuery = searchInput.value;
+    searchButton.addEventListener('click', () => {
+        const searchQuery = searchInput.value;
+        
+        axios.get(apiUrl, {
+            params: {
+                key: apiKey,
+                q: searchQuery,
+                part: 'snippet',
+                maxResults: 10, // Quantidade de resultados desejada
+                type: 'video',  // Apenas vídeos
+            }
+        })
+        .then(response => {
+            const results = response.data.items;
 
-    axios.get(apiUrl, {
-        params: {
-            key: apiKey,
-            q: searchQuery,
-            part: 'snippet',
-            maxResults: 10, // Quantidade de resultados desejada
-            type: 'video',  // Apenas vídeos
-        }
-    })
-    .then(response => {
-        const results = response.data.items;
+            if (results.length === 0) {
+                resultsContainer.innerHTML = 'Nenhum vídeo encontrado.';
+            } else {
+                const videoList = results.map(video => {
+                    const title = video.snippet.title;
+                    const description = video.snippet.description;
+                    const videoId = video.id.videoId;
 
-        if (results.length === 0) {
-            resultsContainer.innerHTML = 'Nenhum vídeo encontrado.';
-        } else {
-            const videoList = results.map(video => {
-                const title = video.snippet.title;
-                const description = video.snippet.description;
-                const videoId = video.id.videoId;
+                    return `
+                        <div>
+                            <h2>${title}</h2>
+                            <p>${description}</p>
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                        </div>
+                    `;
+                });
 
-                return `
-                    <div>
-                        <h2>${title}</h2>
-                        <p>${description}</p>
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-                    </div>
-                `;
-            });
-
-            resultsContainer.innerHTML = videoList.join('');
-        }
-    })
-    .catch(error => {
-        console.error('Erro na solicitação:', error);
+                resultsContainer.innerHTML = videoList.join('');
+            }
+        })
+        .catch(error => {
+            console.error('Erro na solicitação:', error);
+        });
     });
 });
+
 
 })
 .catch(error => {
