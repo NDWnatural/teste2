d41f4691e5efe57b3a1e26fe76ac95b8f2514fba
NDWnatural <134720252+NDWnatural@users.noreply.github.com>
2023-10-30 10:17:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
 // Substitua 'SUA_CHAVE_DE_API_DO_YOUTUBE' pela sua chave de API real
-const apiKey = 'AIzaSyAiw_Ano92YzywXpv_pPPWHNF8txNpOGsk';
+const apiKey = 'SUA_CHAVE_DE_API_DO_YOUTUBE';
 const apiUrl = 'https://www.googleapis.com/youtube/v3/search';
 
-const searchQuery = 'Mr Beast'; // Substitua pelo termo de pesquisa desejado
+const resultsContainer = document.getElementById('results');
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
-axios.get(apiUrl, {
-    params: {
-        key: apiKey,
-        q: searchQuery,
-        part: 'snippet',
-        maxResults: 10, // Quantidade de resultados desejada
-        type: 'video',  // Apenas vídeos
-    }
-})
-.then(response => {
-    const results = response.data.items;
-    const resultsContainer = document.getElementById('results');
+searchButton.addEventListener('click', () => {
+    const searchQuery = searchInput.value;
 
-    if (results.length === 0) {
-        resultsContainer.innerHTML = 'Nenhum vídeo encontrado.';
-    } else {
-        const videoList = results.map(video => {
-            const title = video.snippet.title;
-            const description = video.snippet.description;
-            const videoId = video.id.videoId;
+    axios.get(apiUrl, {
+        params: {
+            key: apiKey,
+            q: searchQuery,
+            part: 'snippet',
+            maxResults: 10, // Quantidade de resultados desejada
+            type: 'video',  // Apenas vídeos
+        }
+    })
+    .then(response => {
+        const results = response.data.items;
 
-            return `
-                <div>
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
-                </div>
-            `;
-        });
+        if (results.length === 0) {
+            resultsContainer.innerHTML = 'Nenhum vídeo encontrado.';
+        } else {
+            const videoList = results.map(video => {
+                const title = video.snippet.title;
+                const description = video.snippet.description;
+                const videoId = video.id.videoId;
 
-        resultsContainer.innerHTML = videoList.join('');
-    }
+                return `
+                    <div>
+                        <h2>${title}</h2>
+                        <p>${description}</p>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                `;
+            });
+
+            resultsContainer.innerHTML = videoList.join('');
+        }
+    })
+    .catch(error => {
+        console.error('Erro na solicitação:', error);
+    });
+});
+
 })
 .catch(error => {
     console.error('Erro na solicitação:', error);
